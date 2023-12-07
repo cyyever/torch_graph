884808e7679e4ee8ae2023320deefdab11e4ba1d
cyy <cyyever@outlook.com>
2023-12-07 16:11:41 +0800

    Fix code

--- cyy_torch_graph/model/__init__.py
+++ cyy_torch_graph/model/__init__.py
@@ -3,7 +3,8 @@ import functools
 from cyy_torch_toolbox.dataset.collection import DatasetCollection
 from cyy_torch_toolbox.factory import Factory
 from cyy_torch_toolbox.ml_type import DatasetType, MachineLearningPhase
-from cyy_torch_toolbox.model import (global_model_evaluator_factory,
+from cyy_torch_toolbox.model import (create_model,
+                                     global_model_evaluator_factory,
                                      global_model_factory)
 from cyy_torch_toolbox.model.repositary import get_model_info
 
@@ -15,16 +16,15 @@ global_model_evaluator_factory.register(DatasetType.Graph, GraphModelEvaluator)
 def get_model(
     model_constructor_info: dict, dataset_collection: DatasetCollection, **kwargs
 ) -> dict:
-    final_model_kwargs: dict = {}
+    final_model_kwargs: dict = kwargs
     if "num_features" not in kwargs:
         final_model_kwargs["num_features"] = dataset_collection.get_original_dataset(
             phase=MachineLearningPhase.Training
         ).num_features
-
-    final_model_kwargs |= kwargs
     return {
-        "model_kwargs": final_model_kwargs,
-        "constructor": model_constructor_info["constructor"],
+        "model": create_model(
+            model_constructor_info["constructor"], **final_model_kwargs
+        )
     }
 
 
