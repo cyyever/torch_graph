b653a3b2f4380bc8c8e3e939baef2becf610627d
cyy <cyyever@outlook.com>
2024-06-25 14:55:51 +0800

    Fix code

---
+++ cyy_torch_graph/dataset/partition.py
@@ -0,0 +1,39 @@
+import torch
+from torch import Tensor
+from torch_geometric.data import Data
+from torch_geometric.typing import pyg_lib
+from torch_geometric.utils import sort_edge_index
+from torch_geometric.utils.sparse import index2ptr
+
+
+def METIS(
+    data: Data, num_parts: int, edge_weight: torch.Tensor | None = None
+) -> Tensor:
+    r"""Partitions a graph data object into multiple subgraphs""
+    .. note::
+
+    Args:
+        data (torch_geometric.data.Data): The graph data object.
+        num_parts (int): The number of partitions.
+        recursive (bool, optional): If set to :obj:`True`, will use multilevel
+            recursive bisection instead of multilevel k-way partitioning.
+            (default: :obj:`False`)
+    """
+    edge_index = data.edge_index
+    assert edge_index is not None
+    num_nodes = data.num_nodes
+    assert num_nodes is not None
+    # Computes a node-level partition assignment vector via METIS.
+
+    # Calculate CSR representation:
+    row, col = sort_edge_index(edge_index, num_nodes=num_nodes)
+    rowptr = index2ptr(row, size=num_nodes)
+
+    # Compute METIS partitioning:
+    cluster = pyg_lib.partition.metis(
+        rowptr=rowptr.cpu(),
+        col=col.cpu(),
+        num_partitions=num_parts,
+        edge_weight=edge_weight,
+    ).to(edge_index.device)
+    return cluster
