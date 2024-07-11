36bdd7aa3a35dd916a4bf2f0c27cad64758aaa06
cyy <cyyever@outlook.com>
2024-07-11 15:05:30 +0800

    Fix code

--- cyy_torch_graph/dataset/collection.py
+++ cyy_torch_graph/dataset/collection.py
@@ -1,6 +1,8 @@
+from typing import Any
+
 from cyy_torch_toolbox import DatasetCollection
 
 
 class GraphDatasetCollection(DatasetCollection):
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Any) -> None:
         super().__init__(add_index=False, **kwargs)
